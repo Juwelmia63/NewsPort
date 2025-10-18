05015d1a84d0ea3557bb31508dee9e67a030f503
Juwelmia63 <designghor5@gmail.com>
2025-10-18 19:41:49 +0600

    project basic setup added

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-5xl font-semibold text-center">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores libero,
+        magni architecto ex aliquam illum quia quod quisquam soluta beatae!
+      </h1>
+    </>
+  );
+}
+
+export default App;
